3413003ce759300a59f5031a712afef242dd1fb9
Kianeay <70838054+Kianeay@users.noreply.github.com>
2021-03-06 18:33:14 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = [];
+  let fig = 0;
+
+  for (let i =0; i < str.length; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+          if (str[i] === bracketsConfig[j][0] && bracketsConfig[j][0] === bracketsConfig[j][1]) {
+              
+          }
+      }
+  }
 }
